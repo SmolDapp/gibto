2dc3214e811a08f990faa92705fa046893fec139
Major <major@ycorpo.com>
2023-05-10 18:19:00 +0200

    fix: remove ded code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import Marquee from 'react-fast-marquee';
 import Link from 'next/link';
 import {useRouter} from 'next/router';
@@ -143,59 +143,6 @@ function	SearchFor(): ReactElement {
 				</div>
 			</div>
 		</form>
-	);
-}
-
-function InfiniteLooper({
-	speed,
-	direction,
-	children
-}: {
-	speed: number;
-	direction: 'right' | 'left';
-	children: React.ReactNode;
-}) {
-	const [looperInstances, setLooperInstances] = useState(1);
-	const outerRef = useRef<HTMLDivElement>(null);
-	const innerRef = useRef<HTMLDivElement>(null);
-
-	const setupInstances = useCallback(() => {
-		if (!innerRef?.current || !outerRef?.current) {
-			return;
-		}
-
-		const {width} = innerRef.current.getBoundingClientRect();
-
-		const {width: parentWidth} = outerRef.current.getBoundingClientRect();
-
-		const instanceWidth = width / innerRef.current.children.length;
-
-		if (width < parentWidth + instanceWidth) {
-			setLooperInstances(looperInstances + Math.ceil(parentWidth / width));
-		}
-	}, [looperInstances]);
-
-	useEffect(() => {
-		setupInstances();
-	}, []);
-
-	return (
-		<div className={'looper'} ref={outerRef}>
-			<div className={'looper__innerList'} ref={innerRef}>
-				{[...Array(looperInstances)].map((_, ind) => (
-					<div
-						key={ind}
-						className={'looper__listInstance'}
-						style={{
-							animationDuration: `${speed}s`,
-							animationDirection: direction === 'right' ? 'reverse' : 'normal'
-						}}
-					>
-						{children}
-					</div>
-				))}
-			</div>
-		</div>
 	);
 }
 
