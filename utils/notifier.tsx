7b46416e04dab2f79f69c22363006f1f857b95f3
Major <major@ycorpo.com>
2023-05-19 11:19:01 +0200

    feat: new gib

--- utils/notifier.tsx
+++ utils/notifier.tsx
@@ -12,7 +12,7 @@ function notify({from, fromName, to, toName, tokenName, amountNormalized, value,
 }): void {
 	axios.post('/api/notify', {
 		messages: [
-			'*❤️ A new gib has been detected*',
+			'*❤️ A new gib has been detected!*',
 			`\t\t\t\t\t\t[${fromName}](https://etherscan.io/address/${from}) sent ${amountNormalized} ${tokenName} (~$${value}) to [${toName}](https://etherscan.io/address/${to})`,
 			`\t\t\t\t\t\t[View on Etherscan](${txLink})`
 		]
