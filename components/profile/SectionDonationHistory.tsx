83cb7b7596375a62e7bcce281ce23641631ed71c
Major <major@ycorpo.com>
2023-07-06 14:12:32 +0200

    fix: explorer

--- components/profile/SectionDonationHistory.tsx
+++ components/profile/SectionDonationHistory.tsx
@@ -4,7 +4,7 @@ import {ImageWithFallback} from 'components/common/ImageWithFallback';
 import IconChevronBoth from 'components/icons/IconChevronBoth';
 import IconCircleCross from 'components/icons/IconCircleCross';
 import IconSpinner from 'components/icons/IconSpinner';
-import {useNetwork} from 'wagmi';
+import {useConnect} from 'wagmi';
 import {Button} from '@yearn-finance/web-lib/components/Button';
 import {useWeb3} from '@yearn-finance/web-lib/contexts/useWeb3';
 import IconLinkOut from '@yearn-finance/web-lib/icons/IconLinkOut';
@@ -21,7 +21,7 @@ function	SectionDonationHistory({donateHistory, isLoading, name}: {
 	isLoading: boolean,
 }): ReactElement {
 	const {address} = useWeb3();
-	const {chains} = useNetwork();
+	const {connectors} = useConnect();
 
 	if (isLoading && donateHistory.length === 0) {
 		return (
@@ -49,76 +49,81 @@ function	SectionDonationHistory({donateHistory, isLoading, name}: {
 	}
 	return (
 		<section>
-			{donateHistory?.map((donation): ReactElement => (
-				<details
-					key={donation.UUID}
-					className={'mb-2 w-full rounded border border-neutral-200'}>
-					<summary className={'relative grid px-4 py-2 md:grid-cols-9 md:px-6'}>
-						<div className={'yearn--table-token-section'}>
-							<div className={'yearn--table-token-section-item'}>
-								<div className={'yearn--table-token-section-item-image'}>
-									<ImageWithFallback
-										key={`${donation.UUID}_${donation.token}`}
-										alt={''}
-										width={40}
-										height={40}
-										quality={90}
-										src={`https://assets.smold.app/api/token/${donation.chainID === 1337 ? 1 : donation.chainID}/${donation.token}/logo-128.png`}
-										loading={'eager'} />
-								</div>
-								<div className={'flex flex-col'}>
-									<p>
-										{formatAmount(donation.amount, 0, 4)}
-										<span className={'text-sm text-neutral-400'}>{` ${donation.tokenName}`}</span>
-									</p>
-									<small className={'font-number text-xs text-neutral-400'}>
-										{`$${formatAmount(donation.value, 0, 2)}`}
-									</small>
-								</div>
-							</div>
-						</div>
+			{donateHistory?.map((donation): ReactElement => {
+				const injectedConnector = connectors.find((e): boolean => e.id === 'injected');
+				const chains = injectedConnector?.chains || [];
 
-						<div className={'yearn--table-data-section md:grid-cols-9'}>
-							<div className={'yearn--table-data-section-item md:col-span-6'}>
-								<p className={'text-start text-xs leading-5 text-neutral-400'}>{'From'}</p>
-								<b className={'yearn--table-data-section-item-value font-number'}>
-									<Link
-										href={`/${donation.from}`}
-										className={'hover:underline'}>
-										{toAddress(donation.from) === toAddress(address) ? 'You' : (donation.fromENS || donation.from)}
-									</Link>
-								</b>
+				return (
+					<details
+						key={donation.UUID}
+						className={'mb-2 w-full rounded border border-neutral-200'}>
+						<summary className={'relative grid px-4 py-2 md:grid-cols-9 md:px-6'}>
+							<div className={'yearn--table-token-section'}>
+								<div className={'yearn--table-token-section-item'}>
+									<div className={'yearn--table-token-section-item-image'}>
+										<ImageWithFallback
+											key={`${donation.UUID}_${donation.token}`}
+											alt={''}
+											width={40}
+											height={40}
+											quality={90}
+											src={`https://assets.smold.app/api/token/${donation.chainID === 1337 ? 1 : donation.chainID}/${donation.token}/logo-128.png`}
+											loading={'eager'} />
+									</div>
+									<div className={'flex flex-col'}>
+										<p>
+											{formatAmount(donation.amount, 0, 4)}
+											<span className={'text-sm text-neutral-400'}>{` ${donation.tokenName}`}</span>
+										</p>
+										<small className={'font-number text-xs text-neutral-400'}>
+											{`$${formatAmount(donation.value, 0, 2)}`}
+										</small>
+									</div>
+								</div>
 							</div>
 
-							<div className={'yearn--table-data-section-item absolute right-4 top-2 md:relative md:col-span-3'}>
-								<div className={'font-number mt-2 flex h-full flex-row items-center justify-end md:-mt-2'}>
-									<p className={'text-xs text-neutral-400'}>
-										{formatDuration((donation.time * 1000) - new Date().valueOf(), true)}
-									</p>
-									<a
-										href={`${chains.find((e): boolean => e.id === donation.chainID)?.blockExplorers?.default?.url || 'https://etherscan.io'}/tx/${donation.txHash}`}
-										target={'_blank'}
-										rel={'noreferrer'}
-										className={'mx-2 text-neutral-400 transition-colors hover:text-neutral-900 md:mx-0'}>
-										<IconLinkOut className={'h-4 w-4 md:ml-4'} />
-									</a>
-									<IconChevronBoth
-										className={`-mr-2 h-4 w-4 text-neutral-400 transition-colors hover:text-neutral-900 md:ml-4 ${donation.message ? '' : 'pointer-events-none hidden opacity-0 md:flex'}`} />
+							<div className={'yearn--table-data-section md:grid-cols-9'}>
+								<div className={'yearn--table-data-section-item md:col-span-6'}>
+									<p className={'text-start text-xs leading-5 text-neutral-400'}>{'From'}</p>
+									<b className={'yearn--table-data-section-item-value font-number'}>
+										<Link
+											href={`/${donation.from}`}
+											className={'hover:underline'}>
+											{toAddress(donation.from) === toAddress(address) ? 'You' : (donation.fromENS || donation.from)}
+										</Link>
+									</b>
+								</div>
+
+								<div className={'yearn--table-data-section-item absolute right-4 top-2 md:relative md:col-span-3'}>
+									<div className={'font-number mt-2 flex h-full flex-row items-center justify-end md:-mt-2'}>
+										<p className={'text-xs text-neutral-400'}>
+											{formatDuration((donation.time * 1000) - new Date().valueOf(), true)}
+										</p>
+										<a
+											href={`${chains.find((e): boolean => e.id === donation.chainID)?.blockExplorers?.default?.url || 'https://etherscan.io'}/tx/${donation.txHash}`}
+											target={'_blank'}
+											rel={'noreferrer'}
+											className={'mx-2 text-neutral-400 transition-colors hover:text-neutral-900 md:mx-0'}>
+											<IconLinkOut className={'h-4 w-4 md:ml-4'} />
+										</a>
+										<IconChevronBoth
+											className={`-mr-2 h-4 w-4 text-neutral-400 transition-colors hover:text-neutral-900 md:ml-4 ${donation.message ? '' : 'pointer-events-none hidden opacity-0 md:flex'}`} />
+									</div>
 								</div>
 							</div>
-						</div>
-					</summary>
-					{donation.message && (
-						<div className={'border-t border-neutral-200 p-4'}>
-							<div className={'border-l-2 border-neutral-300'}>
-								<div className={'bg-neutral-0 p-4'}>
-									<p className={'text-sm text-neutral-700'}>{donation.message}</p>
+						</summary>
+						{donation.message && (
+							<div className={'border-t border-neutral-200 p-4'}>
+								<div className={'border-l-2 border-neutral-300'}>
+									<div className={'bg-neutral-0 p-4'}>
+										<p className={'text-sm text-neutral-700'}>{donation.message}</p>
+									</div>
 								</div>
 							</div>
-						</div>
-					)}
-				</details>
-			))}
+						)}
+					</details>
+				);
+			})}
 
 		</section>
 	);
